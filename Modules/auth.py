de0deb5ecdd63c0a6e6c533a1991c1a9ef980036
CallAFunction <norishimogawa@gmail.com>
2026-03-30 21:17:30 +0100

    added verify function

--- Modules/auth.py
+++ Modules/auth.py
@@ -3,7 +3,9 @@ import bcrypt
 def hash(password):
     hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
     return hashed
-def verify():
-    pass
-    
+
+def verify(password, hashed):
+    return bcrypt.checkpw(password.encode(), hashed)
+
+
     
