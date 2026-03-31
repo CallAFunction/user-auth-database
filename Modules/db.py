7b3f6ca1fae8db63a7f0f8a8e41d3955b05cd206
CallAFunction <norishimogawa@gmail.com>
2026-03-31 21:29:19 +0100

    setup database connection logic

--- Modules/db.py
+++ Modules/db.py
@@ -1,4 +1,14 @@
 import sqlite3
 
+#function for connecting to database
 def connect(database):
-    connection = sqlite3.connect('../accounts.db')
+    connection_cursor = sqlite3.connect(database)
+    return connection_cursor
+
+def new_user(username, password):
+    connection_cursor.execute(
+        f"""
+        INSERT INTO users (username, password)
+        values({username}, {password})
+        """
+    )
