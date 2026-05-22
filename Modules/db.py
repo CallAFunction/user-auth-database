731791c29496e9287fe4923d3ed59ee616d6eba3
CallAFunction <norishimogawa@gmail.com>
2026-05-22 17:31:15 +0100

    Added get_user_password function for getting the user password.

--- Modules/db.py
+++ Modules/db.py
@@ -6,6 +6,7 @@ def connect(database):
     
     return conn
 
+#function for creating new user 
 def new_user(conn,username, password_hash):
     cursor = conn.cursor()
     cursor.execute(
@@ -16,3 +17,18 @@ def new_user(conn,username, password_hash):
         (username, password_hash)
     )
     conn.commit()
+
+def find_user_password(conn, username):
+    cursor = conn.cursor()
+    password = cursor.execute(
+        """
+        SELECT password FROM users
+        WHERE username = ?;
+        """,
+        (username,)
+    )
+    result  = cursor.fetchone()
+    if result is None:
+        return None
+    return result[0]
+    
