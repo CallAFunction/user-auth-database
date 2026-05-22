fd23254ee0238d4a5b344d3976f5b7b6e10fd140
CallAFunction <norishimogawa@gmail.com>
2026-05-22 17:30:52 +0100

    added ciomments to auth.py

--- Modules/auth.py
+++ Modules/auth.py
@@ -1,9 +1,11 @@
 import bcrypt 
 
+#hashes entered password
 def hash(password):
     hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
     return hashed
 
+#function for verifying entered password
 def verify(password, hashed):
     return bcrypt.checkpw(password.encode(), hashed)
  
